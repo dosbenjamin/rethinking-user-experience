98bf6346f0fea1b90cf7f9f9cf83b7a4ca4290a9
Benjamin Dos Santos <dosbenjamin@gmail.com>
2019-01-20 18:27:40 +0100

    add mobile layout + clean css

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,13 @@
-const tableSlider = document.querySelector('.tableSlider');
-tableSlider.classList.add('tableSlider--js');
-sliderTranslate('.imgSlider', '#problem .sliderIndicator__item', 'sliderIndicator__item--onWhiteEnabled');
-sliderTranslate('.tableSlider', '.sliderIndicator__item', 'sliderIndicator__item--enabled');
+const sliderJS = document.querySelectorAll('.slider');
+sliderJS.forEach(element => {
+    element.classList.add('slider--js');
+});
+sliderTranslate('#sliderOne', '#sliderOneIndicator .sliderIndicator__item', 'sliderIndicator__item--enabled');
+sliderTranslate('#sliderTwo', '#sliderTwoIndicator .sliderIndicator__item', 'sliderIndicator__item--onBlackEnabled');
+sliderTranslate('#sliderThree', '#sliderThreeIndicator .sliderIndicator__item', 'sliderIndicator__item--enabled');
 function sliderTranslate(sliderBlock, indicatorItem, indicatorItemEnabled) {
     const slider = document.querySelector(sliderBlock);
     const sliderIndicator = document.querySelectorAll(indicatorItem);
-    // slider.style.width = 'calc(100% + var(--space))'
-    slider.style.overflowX = 'visible';
     let lastTranslateXvalue;
     sliderIndicator.forEach((element, numberOfElementClicked) => {
         element.addEventListener('click', (e) => {
@@ -25,3 +26,14 @@ function sliderTranslate(sliderBlock, indicatorItem, indicatorItemEnabled) {
         });
     });
 }
+const burger = document.querySelector('.burger');
+const menu = document.querySelector('.menu');
+const body = document.body;
+burger.addEventListener('click', (e) => {
+    menu.classList.toggle('menu--open');
+    body.classList.toggle('noscroll');
+    const burgerText = document.querySelectorAll('.burger__item');
+    burgerText.forEach(element => {
+        element.classList.toggle('burger__item--open');
+    });
+});
